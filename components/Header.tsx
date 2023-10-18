5853a7a2ed6782445922bac05b50f4c8000dda8b
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-10-18 04:09:40 -0500

    building emote maker

---
+++ components/Header.tsx
@@ -0,0 +1,94 @@
+"use client"
+
+
+import Link from "next/link";
+// import { signIn, signOut, useSession } from "next-auth/react";
+// import { useBuyCredits } from "~/hooks/useBuyCredits";
+// import { api } from "~/utils/api";
+// import { Button } from "./Button";
+// import { PrimaryLink } from "./PrimaryLink";
+
+
+// import { PrimaryLink } from "./PrimaryLink";
+import { Button } from "./ui/button";
+
+export function Header() {
+//   const session = useSession();
+//   const { buyCredits } = useBuyCredits();
+
+//   const isLoggedIn = !!session.data;
+
+//   const credits = api.user.getCredits.useQuery(undefined, {
+//     enabled: isLoggedIn,
+//   });
+
+  return (
+    <header className="dark:bg-gray-900">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        {/* <PrimaryLink href="/">Icon Generator</PrimaryLink> */}
+        <Button variant="ghost">
+          <Link href="/">TwitchEmotes.ai</Link>
+        </Button>
+        <ul className="flex gap-4">
+          <li>
+            <Button variant="ghost">
+              <Link href="/generate">Generate</Link>
+            </Button>
+          </li>
+          <li>
+          <Button variant="ghost">
+              <Link href="/community">Community</Link>
+            </Button>
+          </li>
+          {/* {isLoggedIn && ( */}
+            <li>
+            <Button variant="ghost">
+              <Link href="/collection">Collection</Link>
+            </Button>
+            </li>
+          {/* )} */}
+        </ul>
+        <ul className="flex gap-4">
+          {/* {isLoggedIn && ( */}
+            <>
+              <div className="flex items-center">
+                Credits remaining 
+                {/* {credits.data} */}
+              </div>
+              <li>
+                <Button
+                  // onClick={() => {
+                  //   buyCredits().catch(console.error);
+                  // }}
+                >
+                  Buy Credits
+                </Button>
+              </li>
+              <li>
+                <Button
+                  variant="secondary"
+                  // onClick={() => {
+                  //   signOut().catch(console.error);
+                  // }}
+                >
+                  Logout
+                </Button>
+              </li>
+            </>
+          {/* )} */}
+          {/* {!isLoggedIn && ( */}
+            <li>
+              <Button
+                // onClick={() => {
+                //   signIn().catch(console.error);
+                // }}
+              >
+                Login
+              </Button>
+            </li>
+          {/* )} */}
+        </ul>
+      </div>
+    </header>
+  );
+}
---
+++ components/PrimaryLinkButton.tsx
@@ -0,0 +1,20 @@
+// // import clsx from "clsx";
+// import Link, { type LinkProps } from "next/link";
+// import { type ReactNode } from "react";
+
+// export function PrimaryLinkButton(
+//   props: LinkProps & { children: ReactNode; className?: string }
+// ) {
+//   const { className, ...propsWithoutClassname } = props;
+
+//   return (
+//     <Link
+//       className={
+//         "rounded bg-blue-400 px-4 py-2 hover:bg-blue-500"
+//       }
+//       {...propsWithoutClassname}
+//     >
+//       {props.children}
+//     </Link>
+//   );
+// }
