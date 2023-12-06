147ee08b3efaf27e9e4fa839c2e73dc2fa413b6f
pauseplayrepeat <mrsqueex@gmail.com>
2023-12-05 20:33:09 -0600

    added different types of emotes that can be generated

--- app/(routes)/create/page.tsx
+++ app/(routes)/create/page.tsx
@@ -14,7 +14,7 @@ import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Card, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 // import { Loader } from "@/components/loader";
 // import { Empty } from "@/components/ui/empty";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
@@ -30,6 +30,7 @@ import { UploadButton } from "@/utils/uploadthing";
 import { FileUpload } from "@/components/FileUpload";
 import ImageToPrompt from "@/components/ImageToPrompt";
 import ChatContainer from "@/components/ChatContainer";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const demophotos = [
   {
@@ -69,10 +70,12 @@ const demophotos = [
 
 // const demophotos = ["/foxemote1.png"]; // Add more image paths as needed
 
+
 const PhotoPage = () => {
 //   const proModal = useProModal();
   const router = useRouter();
   const [photos, setPhotos] = useState<string[]>([]);
+  const [selectedTab, setSelectedTab] = useState("Face");
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -80,7 +83,7 @@ const PhotoPage = () => {
       prompt: "",
       amount: "1",
       resolution: "512x512",
-      // templates: "prompt" // Default to the first template
+      // style: "",
     }
   });
 
@@ -89,20 +92,28 @@ const PhotoPage = () => {
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       setPhotos([]);
-
-      const response = await axios.post('/api/dalle', values);
-
+  
+      let finalPrompt = "";
+  
+      switch (selectedTab) {
+        case "Face":
+          finalPrompt = `Design a vibrant, cartoonish digital emote suitable for use on a Twitch streamer's channel, centered in the image with a solid white background. The emote should depict a single emotion from ${values.prompt}, ensuring it is expressive and visible at a small scale. It should feature exaggerated facial features appropriate for the emotion being conveyed, like excitement or surprise. The style should be playful and friendly, with a distinct, cohesive look.`;
+          break;
+        case "Text":
+          finalPrompt = `Create a dynamic text design in an anime style for a Twitch stream, incorporating the phrase '${values.prompt}'. The design should feature bold, clean lines and vibrant colors, with elements like speed lines or explosive backgrounds typical of anime. The text should be stylized to match the energetic and dramatic flair of anime art.`
+          break;
+        case "Object":
+          finalPrompt = `Design a dynamic icon in an anime style for Twitch, featuring the concept '${values.prompt}'. The icon should embrace the vibrant and dynamic essence of anime, with bold, clean lines and bright, eye-catching colors. Incorporate elements characteristic of anime, such as dramatic speed lines, explosive effects, or stylized motion blur to convey energy and movement. The overall design should reflect the high-energy and drama typical of anime, making it visually striking and instantly recognizable. This icon should capture the essence of the specified concept in a way that resonates with the spirited and expressive world of anime.`
+          break;
+      }
+  
+      const response = await axios.post('/api/dalle', { ...values, prompt: finalPrompt });
+  
       const urls = response.data.map((image: { url: string }) => image.url);
-
+  
       setPhotos(urls);
     } catch (error: any) {
-      if (error?.response?.status === 403) {
-        // proModal.onOpen();
-      } else {
-        // toast.error("Something went wrong.");
-      }
-    } finally {
-    //   router.refresh();
+      // Handle error
     }
   }
 
@@ -132,16 +143,15 @@ const PhotoPage = () => {
               // }
             }}
           /> */}
-
+          <div className="flex items-center">
+<Tabs defaultValue="Face">
+    <TabsList>
+      <TabsTrigger value="Face">Face Emotes</TabsTrigger>
+      <TabsTrigger value="Text">Text Emotes</TabsTrigger>
+      <TabsTrigger value="Object">Object Emotes</TabsTrigger>
+    </TabsList>
+    <TabsContent value="Face">
   <Form {...form}>
-  {/* <Heading
-  title="Image Generation"
-  description="Turn your prompt into an image."
-  Icon={ImageIcon}
-  iconColor="text-pink-700"
-  bgColor="bg-pink-700/10"
-  className="text-center px-3 md:px-6"
-/> */}
     <form 
       onSubmit={form.handleSubmit(onSubmit)} 
       className="
@@ -172,95 +182,90 @@ const PhotoPage = () => {
                 </FormItem>
               )}
             />
-           {/* <FormField
-  control={form.control}
-  name="templates"
-  render={({ field }) => (
-    <Select 
-      onValueChange={field.onChange} 
-      value={field.value} 
-    >
-      <SelectTrigger>
-        <SelectValue defaultValue={field.value || "Select a template"} />
-      </SelectTrigger>
-      <SelectContent>
-        {templates.map((template) => (
-          <SelectItem 
-            key={template.value} 
-            value={template.value}
-          >
-            {template.label}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
-  )}
-/> */}
-            {/* <FormField
-              control={form.control}
-              name="amount"
-              render={({ field }) => (
-                <FormItem className="col-span-12 lg:col-span-2">
-                  <Select 
-                    disabled={isLoading} 
-                    onValueChange={field.onChange} 
-                    value={field.value} 
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue defaultValue={field.value} />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {amountOptions.map((option) => (
-                        <SelectItem 
-                          key={option.value} 
-                          value={option.value}
-                        >
-                          {option.label}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="resolution"
-              render={({ field }) => (
-                <FormItem className="col-span-12 lg:col-span-2">
-                  <Select 
-                    disabled={isLoading} 
-                    onValueChange={field.onChange} 
-                    value={field.value} 
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue defaultValue={field.value} />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {resolutionOptions.map((option) => (
-                        <SelectItem 
-                          key={option.value} 
-                          value={option.value}
-                        >
-                          {option.label}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </FormItem>
-              )}
-            /> */}
             <Button className="col-start-11 col-span-2 w-full flex justify-center" type="submit" disabled={isLoading} size="icon">
   <Wand2 />
 </Button>
           </form>
         </Form>
+        </TabsContent>
+        <TabsContent value="Text">
+  <Form {...form}>
+    <form 
+      onSubmit={form.handleSubmit(onSubmit)} 
+      className="
+        rounded-lg 
+        border 
+        w-full 
+        p-4 
+        px-3 
+        md:px-6 
+        focus-within:shadow-sm
+        grid
+        grid-cols-12
+        gap-2
+      "
+    >
+            <FormField
+              name="prompt"
+              render={({ field }) => (
+                <FormItem className="col-span-8 lg:col-span-6">
+                  <FormControl className="m-0 p-0">
+                    <Input
+                      className="w-full border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                      disabled={isLoading} 
+                      placeholder="Good game!" 
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button className="col-start-11 col-span-2 w-full flex justify-center" type="submit" disabled={isLoading} size="icon">
+  <Wand2 />
+</Button>
+          </form>
+        </Form>
+        </TabsContent>
+        <TabsContent value="Object">
+  <Form {...form}>
+    <form 
+      onSubmit={form.handleSubmit(onSubmit)} 
+      className="
+        rounded-lg 
+        border 
+        w-full 
+        p-4 
+        px-3 
+        md:px-6 
+        focus-within:shadow-sm
+        grid
+        grid-cols-12
+        gap-2
+      "
+    >
+            <FormField
+              name="prompt"
+              render={({ field }) => (
+                <FormItem className="col-span-8 lg:col-span-6">
+                  <FormControl className="m-0 p-0">
+                    <Input
+                      className="w-full border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                      disabled={isLoading} 
+                      placeholder="A bag of coins" 
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button className="col-start-11 col-span-2 w-full flex justify-center" type="submit" disabled={isLoading} size="icon">
+  <Wand2 />
+</Button>
+          </form>
+        </Form>
+        </TabsContent>
+        </Tabs>
+        </div>
         {isLoading && (
           <div className="p-20">
             <Loader />
