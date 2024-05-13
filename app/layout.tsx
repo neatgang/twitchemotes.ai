7f58d97e48c71560650f8ad2539e9b7ff78075bb
pauseplayrepeat <mrsqueex@gmail.com>
2024-05-13 03:29:34 -0500

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,8 @@ import { ToasterProvider } from '@/components/ToasterProvider'
 import { checkSubscription } from '../lib/subscription'
 import { ConvexClientProvider } from '@/providers/canvas/convex-client-provider'
 import { TooltipProvider } from '@/components/ui/tooltip'
+import Head from 'next/head'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +20,8 @@ export const metadata: Metadata = {
   title: 'EmoteMaker.ai',
   description: 'Turn your prompt into an emote, perfect for Twitch Streamers, Discord Moderators, and others.',
 }
+
+
 
 export default async function RootLayout({
   children,
@@ -30,6 +34,16 @@ export default async function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <Script async src="//cdn.trackdesk.com/tracking.js" />
+        <Script
+          id="trackdesk-script"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(t,d,k){(t[k]=t[k]||[]).push(d);t[d]=t[d]||t[k].f||function(){(t[d].q=t[d].q||[]).push(arguments)}})(window,"trackdesk","TrackdeskObject");
+              trackdesk("emotemaker", "click");
+            `,
+          }}
+        />
         <body className={inter.className}>
           <div className="h-full relative">
             <Navbar isPro={isPro} apiLimitCount={apiLimitCount} />
