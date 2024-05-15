96000dc448d85b6a8af3ce0a4658dc6bdfd6d088
pauseplayrepeat <mrsqueex@gmail.com>
2024-05-15 04:41:51 -0500

    updating

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { Client } from '@planetscale/database';
+import { PrismaPlanetScale } from '@prisma/adapter-planetscale';
+import { PrismaClient } from '@prisma/client';
+
+import { fetch as undiciFetch } from 'undici';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const client = new Client({ url: connectionString, fetch: undiciFetch });
+const adapter = new PrismaPlanetScale(client);
 
 declare global {
     var prisma: PrismaClient | undefined;
 };
 
-export const db = globalThis.prisma ?? new PrismaClient();
+export const db = globalThis.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
     globalThis.prisma = db;
