0df26fa4f9030b9d338166578ee0ee513142de01
pauseplayrepeat <mrsqueex@gmail.com>
2024-05-18 03:02:23 -0500

    no more chibi on edge

--- app/api/chibi/route.ts
+++ app/api/chibi/route.ts
@@ -10,7 +10,7 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-export const runtime = "edge"
+// export const runtime = "edge"
 
 export async function POST(
   req: Request
