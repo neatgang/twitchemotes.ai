1d2070739caf713ee4657d4dfde1ed4b93af19c9
pauseplayrepeat <mrsqueex@gmail.com>
2024-05-30 22:20:58 -0500

    updated credits display

--- components/CreditsDisplay.tsx
+++ components/CreditsDisplay.tsx
@@ -2,11 +2,14 @@
 
 
 import { Coins, CoinsIcon } from "lucide-react";
+import Link from "next/link";
 
 export const CreditsDisplay = ({ credits }: { credits: number }) => {
     return (
       <div className="text-sm font-semibold mx-2 flex">
-            <Coins className="mx-2 w-4 h-4 flex" /> <p>{credits}</p>
-            </div>
+        <Link href="/credits">
+          <Coins className="mx-2 w-4 h-4 flex" /> <p>{credits}</p>
+        </Link>
+      </div>
     );
   };
