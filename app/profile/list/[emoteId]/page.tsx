75521b2ff9ceea413424f7699104a99c42ef7ce3
pauseplayrepeat <mrsqueex@gmail.com>
2024-10-01 03:19:40 -0500

    trying to push again

--- app/profile/list/[emoteId]/page.tsx
+++ app/profile/list/[emoteId]/page.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import { auth } from "@clerk/nextjs/server"
-
 import { db } from "@/lib/db";
 import ListEmote from "../../_components/list-emote";
 
@@ -10,25 +10,32 @@ const MarketplacePage = async ({
       emoteId: string;
   };
 }) => {
+  const { userId } = auth();
 
-const { userId } = auth();
+  if (!userId) {
+    return <div>Please sign in to list emotes.</div>;
+  }
 
-const emoteForSale = await db.emoteForSale.findUnique({
+  const emote = await db.emote.findUnique({
     where: {
-      emoteId: params.emoteId,
-      // userId?: userId
+      id: params.emoteId,
+      userId: userId,
     },
-    // orderBy: {
-    //   createdAt: "desc",
-    // }
+    include: {
+      emoteForSale: true,
+    }
   });
 
+  if (!emote) {
+    return <div>Emote not found or you dont have permission to list it.</div>;
+  }
 
-    return (
-        <>
-            <ListEmote emotesForSale={emoteForSale}/>
-        </>
-    )
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">List Your Emote</h1>
+      <ListEmote emoteForSale={emote.emoteForSale} emote={emote} />
+    </div>
+  )
 }
 
 export default MarketplacePage
