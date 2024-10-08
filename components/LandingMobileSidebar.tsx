ad8f9091e022b6d8ad70c2139f5fa5553514b7b1
pauseplayrepeat <mrsqueex@gmail.com>
2024-10-08 00:20:12 -0500

    updated stripe route and mobile sidebar

--- components/LandingMobileSidebar.tsx
+++ components/LandingMobileSidebar.tsx
@@ -1,47 +1,52 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import { Montserrat } from 'next/font/google'
-import { BookIcon, Code, ComputerIcon, ImageIcon, LayoutDashboard, MessageSquare, Music, Paintbrush2, Settings, StoreIcon, UserIcon, VideoIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
-
 import { cn } from "@/lib/utils";
+import { Paintbrush2, StoreIcon, LayoutDashboard } from "lucide-react";
 
 const poppins = Montserrat ({ weight: '600', subsets: ['latin'] });
 
 const routes = [
-    {
-      label: 'Create Emotes',
-      icon: Paintbrush2, // Example icon, replace with actual icons as needed
-      href: '/emoteboard/editor/1',
-    },
-    {
-      label: 'Showcase',
-      icon: StoreIcon, // Example icon
-      href: '/showcase',
-    },
-  ];
+  {
+    label: 'Emoteboard',
+    icon: LayoutDashboard,
+    href: '/emoteboard/editor/1',
+  },
+  {
+    label: 'Marketplace',
+    icon: StoreIcon,
+    href: '/marketplace',
+  },
+];
 
-export const LandingMobileSidebar = ({
-}) => {
+export const LandingMobileSidebar = () => {
   const pathname = usePathname();
 
   return (
-    <div className="space-y-4 py-4 flex flex-col h-full">
+    <div className="space-y-4 py-4 flex flex-col h-full bg-white">
       <div className="px-3 py-2 flex-1">
+        <Link href="/" className="flex items-center pl-3 mb-14">
+          <div className="relative w-8 h-8 mr-4">
+            <img src="/peepopainter.jpg" alt="Logo" className="rounded-full" />
+          </div>
+          <h1 className={cn("text-2xl font-bold", poppins.className)}>
+            EmoteMaker.ai
+          </h1>
+        </Link>
         <div className="space-y-1">
           {routes.map((route) => (
             <Link
               key={route.href} 
               href={route.href}
               className={cn(
-                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-black hover:bg-white/10 rounded-lg transition",
-                pathname === route.href ? "bg-white/10" : "text-zinc-400",
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-black hover:bg-gray-100 rounded-lg transition",
+                pathname === route.href ? "text-black bg-gray-100" : "text-zinc-400",
               )}
             >
               <div className="flex items-center flex-1">
-                {/* <route.icon className={cn("h-5 w-5 mr-3", route.color)} /> */}
+                <route.icon className={cn("h-5 w-5 mr-3")} />
                 {route.label}
               </div>
             </Link>
