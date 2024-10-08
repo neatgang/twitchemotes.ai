f8eb4a595efc5399b4ad6eec01aee07211966aa8
pauseplayrepeat <mrsqueex@gmail.com>
2024-10-08 03:17:50 -0500

    update dashboard and prisma schema

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,16 +4,29 @@ import { DashboardSkeleton } from './_components/DashboardSkeleton'
 import { getUserCredits } from "@/actions/get-user-credits"
 import { checkSubscription } from "@/lib/subscription"
 import { redirect } from 'next/navigation'
-
-import { ProfileFormDialog } from './_components/ProfileFormSheet' // Update this line
+import { ProfileFormDialog } from './_components/ProfileFormSheet'
 import { getProfile } from '@/actions/get-profile'
+import { auth } from "@clerk/nextjs/server"
+import { db } from "@/lib/db"
 
 export default async function DashboardPage() {
+  const { userId } = auth()
+  if (!userId) {
+    redirect('/sign-in')
+  }
+
   const credits = await getUserCredits()
   const isPro = await checkSubscription()
-  const profile = await getProfile() // Fetch the user's profile
+  const profile = await getProfile()
 
-  if (!isPro) {
+  // Fetch user to check admin status
+  const user = await db.user.findUnique({
+    where: { id: userId },
+    select: { isAdmin: true }
+  })
+
+  // If user is not pro and not an admin, redirect to pricing
+  if (!isPro && !user?.isAdmin) {
     redirect('/pricing')
   }
 
